bf163e38e3b732ce9d4d75a058d9e74647affb07
WiktorK1234 <szczecinskiwiktor@gmail.com>
2025-04-19 12:58:43 +0200

    dodanie progresje poziomow oraz zdobywanie zlota

--- giera/RPG.cpp
+++ giera/RPG.cpp
@@ -30,6 +30,9 @@ public:
     float szansaNaUnikBierny;
     float unikManualny;
     float szansaNaUcieczke;
+    int poziom = 1;
+    int exp = 0;
+    int zloto=0;
 
     Postac() = default;
 
@@ -42,6 +45,19 @@ public:
         cout << "Szansa na unik bierny: " << szansaNaUnikBierny * 100 << "%\n";
         cout << "Unik manualny: " << unikManualny * 100 << "%\n";
         cout << "Szansa na ucieczke: " << szansaNaUcieczke * 100 << "%\n";
+        cout << "Doswiadczenie: " << exp << " / " << poziom * 100 << "\n";
+         cout << "Zloto: " << zloto << "\n";
+    }
+    
+    void zdobadzDoswiadczenie(int ilosc) {
+        exp += ilosc;
+        while (exp >= poziom * 100 && poziom < 10) {
+            exp -= poziom * 100;
+            poziom++;
+            hp += 10;
+            obrazenia += 2;
+            cout << "Awans! Osiagnieto poziom " << poziom << "! (+10 HP, +2 obrazenia, +50 zlota)\n";
+        }
     }
 
     bool czyUnikBierny() {
@@ -94,9 +110,16 @@ class Przeciwnik {
 public:
     string nazwa;
     int hp;
-    float pancerz;
+    int maxHp; // <-- dodaj to, jeœli nie istnieje
     int obrazenia;
+    float pancerz;
     float szansaNaUnik;
+    int poziom;
+
+    Przeciwnik(string n, int h, int dmg, int lvl)
+        : nazwa(n), hp(h), maxHp(h), obrazenia(dmg), poziom(lvl) {}
+        
+     Przeciwnik() : nazwa("Nieznany"), hp(0), maxHp(0), obrazenia(0), poziom(1) {}
 
     virtual void wyswietlStatystyki() {
         cout << "Statystyki przeciwnika (" << nazwa << "):\n";
@@ -147,12 +170,12 @@ vector<Przeciwnik*> losujGrupePrzeciwnikow() {
     int typPrzeciwnika = rand() % 2;
     
     if (typPrzeciwnika == 0) { // Szlamy
-        liczbaPrzeciwnikow = 3 + rand() % 3; // 3-5 szlamÃ³w
+        liczbaPrzeciwnikow = 3 + rand() % 3; // 3-5 szlamów
         for (int i = 0; i < liczbaPrzeciwnikow; i++) {
             grupa.push_back(new Szlam());
         }
-    } else { // WilkoÅ‚aki
-        liczbaPrzeciwnikow = 1 + rand() % 2; // 1-2 wilkoÅ‚aki
+    } else { // Wilko³aki
+        liczbaPrzeciwnikow = 1 + rand() % 2; // 1-2 wilko³aki
         for (int i = 0; i < liczbaPrzeciwnikow; i++) {
             grupa.push_back(new Wilkolak());
         }
@@ -199,6 +222,10 @@ Postac wybierzPostac() {
 }
 
 void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
+    int exp = 0;
+    int poziom = 1;
+    int zloto = 0;
+
     while (gracz.hp > 0 && !przeciwnicy.empty()) {
         clearScreen();
         cout << "\n--- Tura gracza (" << gracz.nazwa << ") ---\n";
@@ -216,16 +243,29 @@ void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
         bool akcjaWykonana = false;
         if (wybor == 1) {
             cout << gracz.nazwa << " atakuje!\n";
+            cout << "Wybierz przeciwnika do ataku:\n";
             for (size_t i = 0; i < przeciwnicy.size(); ++i) {
-                Przeciwnik* przeciwnik = przeciwnicy[i];
-                if (przeciwnik->czyUnik()) {
-                    cout << "Przeciwnik " << przeciwnik->nazwa << " udalo sie uniknac ataku!\n";
-                } else {
-                    int obrazeniaGracza = gracz.obliczObrazenia(gracz.obrazenia);
-                    przeciwnik->hp -= obrazeniaGracza;
-                    cout << gracz.nazwa << " zadal " << obrazeniaGracza << " obrazen przeciwnikowi " << przeciwnik->nazwa << "!\n";
-                }
+                cout << i + 1 << ". " << przeciwnicy[i]->nazwa << " (HP: " << przeciwnicy[i]->hp << ")\n";
             }
+            int wyborPrzeciwnika;
+            cin >> wyborPrzeciwnika;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            while (wyborPrzeciwnika < 1 || wyborPrzeciwnika > static_cast<int>(przeciwnicy.size())) {
+                cout << "Nieprawidlowy wybor. Sprobuj ponownie: ";
+                cin >> wyborPrzeciwnika;
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+
+            Przeciwnik* cel = przeciwnicy[wyborPrzeciwnika - 1];
+            if (cel->czyUnik()) {
+                cout << "Przeciwnik " << cel->nazwa << " udalo sie uniknac ataku!\n";
+            } else {
+                int obrazeniaGracza = gracz.obliczObrazenia(gracz.obrazenia);
+                cel->hp -= obrazeniaGracza;
+                cout << gracz.nazwa << " zadal " << obrazeniaGracza << " obrazen przeciwnikowi " << cel->nazwa << "!\n";
+            }
+
             akcjaWykonana = true;
         } else if (wybor == 2) {
             if (gracz.czyUnikManualny()) {
@@ -251,7 +291,7 @@ void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
             clearScreen();
             for (size_t i = 0; i < przeciwnicy.size(); ++i) {
                 przeciwnicy[i]->wyswietlStatystyki();
-                cout<<endl;
+                cout << endl;
             }
             czekajNaKlawisz();
             continue;
@@ -267,9 +307,48 @@ void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
         for (size_t i = 0; i < przeciwnicy.size(); ++i) {
             if (przeciwnicy[i]->hp <= 0) {
                 cout << "Przeciwnik " << przeciwnicy[i]->nazwa << " zostal pokonany!\n";
+
+                int zdobyteExp = 0;
+                if (przeciwnicy[i]->nazwa == "Wilkolak") {
+                    zdobyteExp = 30;
+                } else if (przeciwnicy[i]->nazwa == "Szlam") {
+                    zdobyteExp = 15;
+                }
+
+                exp += zdobyteExp;
+
+                // Z³oto – sensowny wzór
+                int losowyBonus = rand() % 11 + 5; // 5–15%
+                int obrazenia = przeciwnicy[i]->obrazenia;
+                int maxHp = przeciwnicy[i]->maxHp;
+                int poziomPrzeciwnika = przeciwnicy[i]->poziom;
+
+                int bazaZloto = poziomPrzeciwnika * (obrazenia + maxHp / 2);
+                int zdobyteZloto = bazaZloto + (bazaZloto * losowyBonus / 100);
+
+                zloto += zdobyteZloto;
+
+                cout << "Zdobyles " << zdobyteExp << " EXP oraz " << zdobyteZloto << " zlota!\n";
+
+                // Usuwanie przeciwnika
                 delete przeciwnicy[i];
                 przeciwnicy.erase(przeciwnicy.begin() + i);
-                --i; // Zmniejszamy indeks, aby nie pominÄ…Ä‡ nastÄ™pnego przeciwnika po usuniÄ™ciu
+                --i;
+
+                // Sprawdzenie awansu poziomu
+                int wymaganeExp = poziom * 100;
+                while (exp >= wymaganeExp && poziom < 10) {
+                    exp -= wymaganeExp;
+                    poziom++;
+                    cout << "Awansowales na poziom " << poziom << "!\n";
+
+                    // Skalowanie statystyk gracza
+                    gracz.hp += 10;
+                    gracz.obrazenia += 2;
+
+                    cout << "Zyskales 10 HP, 2 obrazenia i 50 zlota!\n";
+                    wymaganeExp = poziom * 100;
+                }
             }
         }
 
@@ -281,7 +360,7 @@ void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
         czekajNaKlawisz();
 
         clearScreen();
-        cout << "\n--- Tura przeciwnikÃ³w ---\n";
+        cout << "\n--- Tura przeciwników ---\n";
         for (size_t i = 0; i < przeciwnicy.size(); ++i) {
             Przeciwnik* przeciwnik = przeciwnicy[i];
             if (gracz.czyUnikBierny()) {
@@ -306,23 +385,34 @@ void walka(Postac &gracz, vector<Przeciwnik*> &przeciwnicy) {
 
         czekajNaKlawisz();
     }
+
+    // === PODSUMOWANIE PO WALCE ===
+    if (gracz.hp > 0 && przeciwnicy.empty()) {
+        cout << "\n--- Podsumowanie walki ---\n";
+        cout << "Poziom postaci: " << poziom << "\n";
+        cout << "Doswiadczenie: " << exp << "/" << (poziom * 100) << "\n";
+        cout << "HP: " << gracz.hp << "\n";
+        cout << "Obrazenia: " << gracz.obrazenia << "\n";
+        cout << "Zloto: " << zloto << "\n";
+    }
 }
+
+
 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
 
     vector<Przeciwnik*> przeciwnicy = losujGrupePrzeciwnikow();
-Postac gracz = wybierzPostac();
-walka(gracz, przeciwnicy);
+    Postac gracz = wybierzPostac();
+    walka(gracz, przeciwnicy);
 
-    if (gracz.hp <= 0) {
-        cout << "\nKoniec gry â€“ postaÄ‡ zostaÅ‚a pokonana!" << endl;
-   }
+    if (gracz.hp <= 0)
+        cout << "\nKoniec gry – postaæ zosta³a pokonana!" << endl;
 
-// Czyszczenie pamiÄ™ci (jeÅ›li nie uÅ¼ywasz smart pointerÃ³w)
-for (size_t i = 0; i < przeciwnicy.size(); ++i) {
-    delete przeciwnicy[i];
-}
+    // Czyszczenie pamiêci (jeœli nie u¿ywasz smart pointerów)
+    for (size_t i = 0; i < przeciwnicy.size(); ++i)
+        delete przeciwnicy[i];
 
     return 0;
 }
+
